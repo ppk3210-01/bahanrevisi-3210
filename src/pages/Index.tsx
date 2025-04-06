2a564e19ffb5a8f0f0f15221633eede50da15c78
andrieskurniawan <ppk.bps3210@gmail.com>
2025-04-06 18:40:21 +0700

    kanan

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -26,7 +26,7 @@ const Index = () => {
       <footer className="bg-gradient-to-r from-blue-600 to-indigo-700 text-white border-t mt-6">
         <div className="container mx-auto py-3 text-center">
           <div className="text-sm font-medium">© 2025 bahanrevisi-3210</div>
-          <div className="text-xs opacity-80">Powered by Andries-3210</div>
+          <div className="text-xs opacity-80 text-right">Powered by Andries-3210</div>
         </div>
       </footer>
     </div>
