972c53da3ef787d00fc89ef9be90db97c7e749e5
PPK3210 <kreasi.ppk3210@gmail.com>
2025-04-14 14:21:46 +0700

    narasi

--- src/components/BudgetComparison.tsx
+++ src/components/BudgetComparison.tsx
@@ -317,7 +317,7 @@ const BudgetComparison: React.FC = () => {
                       <div className="mb-6 p-4 bg-slate-50 border border-slate-200 rounded-md" ref={changesContentRef}>
                         <h4 className="text-lg font-medium mb-2 text-blue-600 text-left">Kesimpulan</h4>
                         <p className="text-slate-700 mb-2 text-left">
-                          Berdasarkan hasil analisis revisi anggaran, total pagu anggaran semula sebesar Rp {totalSemula.toLocaleString('id-ID')} mengalami perubahan menjadi Rp {totalMenjadi.toLocaleString('id-ID')}, dengan selisih Rp {totalSelisih.toLocaleString('id-ID')} atau {totalSelisih === 0 ? 'tetap' : totalSelisih > 0 ? 'meningkat' : 'menurun'}.
+                          Berdasarkan hasil analisis usulan revisi anggaran, total pagu anggaran semula sebesar Rp {totalSemula.toLocaleString('id-ID')} menjadi Rp {totalMenjadi.toLocaleString('id-ID')}, dengan selisih Rp {totalSelisih.toLocaleString('id-ID')} atau {totalSelisih === 0 ? 'pagu tetap' : totalSelisih > 0 ? 'pagu bertambah' : 'pagu berkurang'}.
                         </p>
                         <p className="text-slate-700 mb-2 text-left">
                           Perubahan ini terdiri dari:
